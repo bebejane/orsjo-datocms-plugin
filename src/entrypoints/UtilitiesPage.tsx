2d3f564c9fbd9a1839778309f5892547a6a2a0fb
Bebe Jane <bjornthief@gmail.com>
2022-02-23 19:45:05 +0100

    deploy

--- src/entrypoints/UtilitiesPage.tsx
+++ src/entrypoints/UtilitiesPage.tsx
@@ -2,9 +2,7 @@ import { RenderPageCtx } from 'datocms-plugin-sdk';
 import { Canvas, Button, Spinner, Section } from 'datocms-react-ui';
 import styles from './UtilitiesPage.module.css'
 
-type PropTypes = {
-  ctx: RenderPageCtx,
-};
+type PropTypes = { ctx: RenderPageCtx };
 
 export default function UtilitiesPage({ ctx }: PropTypes) {
   const parameters = ctx.plugin.attributes.parameters as Object;
