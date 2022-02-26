90c0b8acfb8b5907bdc69092dbcad13a5c1b16fa
Bebe Jane <bjornthief@gmail.com>
2022-02-26 17:04:03 +0100

    deploy

--- src/entrypoints/UtilitiesPage.tsx
+++ src/entrypoints/UtilitiesPage.tsx
@@ -1,12 +1,23 @@
+import styles from './UtilitiesPage.module.css'
+import io from 'socket.io-client'
+import { useRef, useEffect } from 'react'
 import { RenderPageCtx } from 'datocms-plugin-sdk';
 import { Canvas, Button, Spinner, Section } from 'datocms-react-ui';
-import styles from './UtilitiesPage.module.css'
+
 
 type PropTypes = { ctx: RenderPageCtx };
 
 export default function UtilitiesPage({ ctx }: PropTypes) {
   const parameters = ctx.plugin.attributes.parameters as Object;
-  
+  const socketRef = useRef();
+
+  useEffect(() => {
+    const socket = io('http://104.248.32.196') 
+    socket.on('log', (log : String) => { 
+      console.log(log)
+    })
+  }, [])
+
   return (
     <Canvas ctx={ctx}>
       <main className={styles.container}>
