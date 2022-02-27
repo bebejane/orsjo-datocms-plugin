7faea6da11cd8361b794f1a58e2ca82497bc0d3e
Bebe Jane <bjornthief@gmail.com>
2022-02-27 11:50:30 +0100

    deploy

--- src/entrypoints/UtilitiesPage.tsx
+++ src/entrypoints/UtilitiesPage.tsx
@@ -5,15 +5,15 @@ import { RenderPageCtx } from 'datocms-plugin-sdk';
 import { Canvas, Button, Spinner, Section } from 'datocms-react-ui';
 
 type PropTypes = { ctx: RenderPageCtx };
+type ValidParameters = { host: string, username: string, password: string };
 
 export default function UtilitiesPage({ ctx }: PropTypes) {
-  const parameters = ctx.plugin.attributes.parameters as Object;
-  const socketRef = useRef();
-  const websocketServer = 'http://104.248.32.196/';
+  const parameters = ctx.plugin.attributes.parameters as ValidParameters;
+  const websocketServer = parameters.host;
 
   useEffect(() => {
     console.log(`Connecting to ${websocketServer}...`);
-    const socket = io(websocketServer, {transports: ['polling', 'websocket']});
+    const socket = io(websocketServer, {transports: ['websocket']});
     socket.on('connect', () => console.log('connected'));
     socket.on('log', (log : String) => { console.log(log)})
     socket.on("connect_error", (err) => console.log(err.toString()));
