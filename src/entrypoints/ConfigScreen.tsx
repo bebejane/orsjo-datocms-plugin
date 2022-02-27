ec8313b1a0bcdcae6178dc7910961eb381ed1446
Bebe Jane <bjornthief@gmail.com>
2022-02-27 11:36:20 +0100

    deploy

--- src/entrypoints/ConfigScreen.tsx
+++ src/entrypoints/ConfigScreen.tsx
@@ -1,5 +1,6 @@
 import { RenderConfigScreenCtx } from 'datocms-plugin-sdk';
 import { Canvas, Form, FieldGroup, TextField, Button } from 'datocms-react-ui';
+import { useState } from 'react';
 import s from './styles.module.css';
 
 type Props = { ctx: RenderConfigScreenCtx; };
@@ -12,15 +13,9 @@ type Parameters = FreshInstallationParameters | ValidParameters;
 
 export default function ConfigScreen({ ctx }: Props) {
   const parameters = ctx.plugin.attributes.parameters as Parameters;
+  const [form, setForm] = useState({...parameters})
 
-  const saveSettings = () => {
-    const { value : host } = document.getElementById('host') as HTMLInputElement;
-    const { value : username } = document.getElementById('username') as HTMLInputElement;
-    const { value : password } = document.getElementById('password') as HTMLInputElement;
-    
-    ctx.updatePluginParameters({ host, username, password })
-   
-  }
+  const saveSettings = () => ctx.updatePluginParameters({...form})
 
   return (
     <Canvas ctx={ctx}>
@@ -31,30 +26,30 @@ export default function ConfigScreen({ ctx }: Props) {
             name="host"
             id="host"
             label="Server URL"
-            value={parameters.host}
+            value={form.host}
             placeholder="http://..."
             hint="Provide webserver host"
-            onChange={(newValue) => ctx.updatePluginParameters({ host: newValue })}
+            onChange={(host) => setForm({...form, host})}
           />
           <TextField
             required
             name="username"
             id="username"
             label="Username"
-            value={parameters.username}
+            value={form.username}
             placeholder="Basic auth username..."
             hint="Provide username"
-            onChange={(newValue) => ctx.updatePluginParameters({ username: newValue })}
+            onChange={(username) => setForm({...form, username})}
           />
           <TextField
             required
             name="password"
             id="password"
             label="Password"
-            value={parameters.password}
+            value={form.password}
             placeholder="Basic auth password..."
             hint="Provide password"
-            onChange={(newValue) => ctx.updatePluginParameters({ password: newValue })}
+            onChange={(password) => setForm({...form, password})}
           />
           <Button onClick={saveSettings}>Save settings</Button>
         </FieldGroup>
